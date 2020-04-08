5c850ee2184c2190d1e419a8e1c47a6443540723
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2020-04-08 18:46:49 -0300

    Biblioteca react-google-button

--- login_screen/src/App.js
+++ login_screen/src/App.js
@@ -3,12 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
+import GoogleButton from 'react-google-button'
 
 
 function App() {
   return (
-    
-<Button />  );
-}
+   
+  //<Button />  );
+
+
+  <GoogleButton
+  onClick={() => { console.log('Google button clicked')}}/> );
+
+  }
+ 
 
 export default App;
