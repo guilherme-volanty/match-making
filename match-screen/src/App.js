2906fffee9c8897e32e9dc0455423126e66a6292
alyssonprr7 <alyssonpatrickregis@gmail.com>
2020-04-08 10:15:34 -0300

    adicionando WebmotorsCard

---
+++ match-screen/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WebmotorsCard from './Components/WebmotorsCard'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <WebmotorsCard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ match-screen/src/Components/WebmotorsCard.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
+import Card from "react-bootstrap/Card"
+import "../Style/WebmotorsCard.css"
+
+
+
+const WebmotorsCard = () => {
+
+    return(
+        <div>
+            <Card border="secondary" style={{ height:"400px", marginTop:"10px", width: '350px' }}>
+            <Card.Body>
+                <Card.Title className = "title">Webmotors</Card.Title>
+                <Card.Text>
+                <div className= "attributes">
+                    <div className = "row-attributes">
+                        <div className ="attribute1">
+                            <p> Marca </p>
+                            <span> Ford</span>
+                        </div>
+                        <div className = "attribute2">
+                            <p> Modelo </p>
+                            <span> Fiesta</span>
+                        </div>
+                    </div>
+                    <div className = "row-attributes">
+                        <div className ="attribute3">
+                            <p> Ano </p>
+                            <span> 2017</span>
+                        </div>
+                        <div className = "attribute4">
+                            <p> Carroceria </p>
+                            <span> Sedã</span>
+                        </div>
+                    </div>
+                    <div className = "attribute5">
+                            <p> Versão </p>
+                            <span> Todas informações </span>
+                    </div>
+                </div>
+            </Card.Text>
+            </Card.Body>
+            </Card>
+        
+        </div>
+    )
+
+}
+
+
+export default WebmotorsCard; 
