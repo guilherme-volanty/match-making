cec9dabdba1a3f20b4b855446de858a622d79967
Thiago Lima <thlima.silva.souza@gmail.com>
2020-04-08 22:39:08 -0300

    corrigindo ponto e virgula no import do firebase

--- login_screen/src/index.js
+++ login_screen/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as firebase from "firebase"
+import * as firebase from "firebase";
 import * as serviceWorker from './serviceWorker';
 
 //Import da configuração do firebase
 import firebaseConfig from "./firebase.config";
 
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
