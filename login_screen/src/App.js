a4c30492fd7fa2b10aaf7f5bb8c0250079a84638
Thiago Lima <thlima.silva.souza@gmail.com>
2020-04-08 21:26:50 -0300

    criando o card de login

--- login_screen/src/App.js
+++ login_screen/src/App.js
@@ -2,14 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
 import GoogleButton from 'react-google-button'
+import {Card, Container} from "react-bootstrap";
+import Row from "react-bootstrap/Row";
 
 
 function App() {
   return (
-    <GoogleButton onClick={() => { console.log('Google button clicked')}}/>
-  );
+    <Container>
+        <Row className="justify-content-md-center">
+      <Card style={{ width: '18rem' }}>
+          <Card.Header>Car Match</Card.Header>
+        <Card.Body>
+          <Card.Subtitle className="mb-2 text-muted">Login com google</Card.Subtitle>
+          <Card.Text>
+            <GoogleButton onClick={() => { console.log('Google button clicked')}}/>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+        </Row>
+    </Container>
+    );
 
   }
  
