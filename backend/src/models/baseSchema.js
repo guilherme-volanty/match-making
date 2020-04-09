01dce15cc21e33ddd6a22a7e192f54aede6f68a2
moraesjeremias <moraesjeremias@gmail.com>
2020-04-09 17:56:48 -0300

    Mongoose Schema

---
+++ backend/src/models/baseSchema.js
@@ -0,0 +1,18 @@
+
+
+const mongoose = require('mongoose');
+
+const BaseSchema = new mongoose.Schema({
+    fipePrice: Number,
+    minPrice: Number,
+    averagePrice: Number,
+    maximumPrice: Number,
+    brand: String,
+    model: String,
+    version: String,
+    modelYear: Number,
+    bodyWork: String,
+    versionId: String, 
+});
+
+module.exports = mongoose.model('BaseCSV', BaseSchema)
