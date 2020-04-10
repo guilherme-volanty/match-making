f908cfa0d66b3defa0a7660dadae6e4e00e7cfe9
Thiago Lima <thlima.silva.souza@gmail.com>
2020-04-10 15:55:34 -0300

    implementando cookie para exibir dados do usuário logado. apaguei comentário no css da boxgoogle

--- login_screen/src/Auth.js
+++ login_screen/src/Auth.js
@@ -1,5 +1,6 @@
 import * as firebase from "firebase";
 import 'firebase/auth';
+import Cookies from 'js-cookie';
 
 //Função de autenticação chamada no onclick do botão de login
 function authenticate() {
@@ -7,6 +8,7 @@ function authenticate() {
     var provider = new firebase.auth.GoogleAuthProvider();
     provider.addScope('profile');
     provider.addScope('email');
+
 
     provider.setCustomParameters({
         provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -18,6 +20,11 @@ function authenticate() {
 
 
     firebase.auth().signInWithPopup(provider).then(function (result) {
+        Cookies.set("user", result.user.displayName);
+        Cookies.set("email", result.user.email);
+        Cookies.set("photo", result.user.photoURL);
+        console.log(Cookies.get());
+
         if (!result.user.email.match(/.*@volanty.com$/)) {
             alert("Não é possivel efetuar login com emails que não sejam do domínio @volanty");
             firebase.auth().currentUser.delete().then(r => console.log("Usuário Deletado/Não autenticado"));
