16b2919f5e3cdff51b4b8341328d5bc53b6caff9
moraesjeremias <moraesjeremias@gmail.com>
2020-04-11 18:24:26 -0300

    React Frontend init

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './components/header/index';
+import Upload from './components/upload/index';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Upload />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/header/index.js
@@ -0,0 +1,19 @@
+import React  from 'react';
+import './styles.css';
+import volantyLogo from '../../assets/volanty-logo.svg';
+
+
+const Header = () => {
+    return <nav className="menu-nav">
+        <div className="brand">
+            <img className="logo" src={volantyLogo} alt="Volanty brand logo"/>
+        </div>
+        <div className="entry">
+            <p>Home</p>
+            <p>Bases</p>
+            <p>Match</p>
+        </div>
+    </nav>
+}
+
+export default Header; 
