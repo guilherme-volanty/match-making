ed5d3205d7c9cda27ce178eebd8b4949695ba8db
moraesjeremias <moraesjeremias@gmail.com>
2020-04-13 10:48:36 -0300

    CORS backend addition

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const useRoutes = require('./routes/routes');
 const dbConnect = require('./db/mongoDB');
+const cors = require('cors');
 
 const app = express();
 
 dbConnect();
 app.use(bodyParser.json());
-
+app.use(cors());
 app.use(useRoutes);
 
 
