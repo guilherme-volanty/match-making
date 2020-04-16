05188506da65cca63dbef5eb8c7502c9e6a7e8df
moraesjeremias <moraesjeremias@gmail.com>
2020-04-16 12:57:55 -0300

    Changes in Mongoose BaseCSV Schema Model

--- backend/src/models/baseSchema.js
+++ backend/src/models/baseSchema.js
@@ -1,17 +1,22 @@
 const mongoose = require('mongoose');
 
 const BaseSchema = new mongoose.Schema({
-    fipePrice: Number,
-    minPrice: Number,
-    averagePrice: Number,
-    maximumPrice: Number,
-    brand: String,
-    model: String,
-    version: String,
-    modelYear: Number,
-    bodyWork: String,
-    versionId: String,
-    isDeleted: false 
+    createDate: Date,
+    updateDate: Date,
+    deletedDate: Date,
+    isDeleted: false,
+    csvObjects:{
+        fipePrice: Number,
+        minPrice: Number,
+        averagePrice: Number,
+        maximumPrice: Number,
+        brand: String,
+        model: String,
+        version: String,
+        modelYear: Number,
+        bodyWork: String,
+        versionId: String,
+    }
 });
 
-module.exports = mongoose.model('BaseCSV', BaseSchema, "base-csv-test")
+module.exports = mongoose.model('BaseCSV', BaseSchema, "1")
--- backend/src/services/keyTransform.js
+++ backend/src/services/keyTransform.js
@@ -1,18 +1,23 @@
 
 function keyTransform(csvObject) {
     return {
-        fipePrice: csvObject.precoFipe,
-        minPrice: csvObject.precoMinimo,
-        averagePrice: csvObject.precoMedio,
-        maximumPrice: csvObject.precoMaximo,
-        brand: csvObject.brand,
-        model: csvObject.model,
-        version: csvObject.version,
-        modelYear: csvObject.modelYear,
-        bodyWork: csvObject.carroceria,
-        versionId: csvObject.versionId,
+        createDate: new Date,
+        updateDate: null,
+        deletedDate: null, 
         isDeleted: false,
+        csvObjects: {
+            fipePrice: csvObject.precoFipe,
+            minPrice: csvObject.precoMinimo,
+            averagePrice: csvObject.precoMedio,
+            maximumPrice: csvObject.precoMaximo,
+            brand: csvObject.brand,
+            model: csvObject.model,
+            version: csvObject.version,
+            modelYear: csvObject.modelYear,
+            bodyWork: csvObject.carroceria,
+            versionId: csvObject.versionId,
+        }
     }
-}
+};
 
 module.exports = keyTransform;
