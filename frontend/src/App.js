1fc6935a5e89f37b2577d623e53cc4a3a6227506
moraesjeremias <moraesjeremias@gmail.com>
2020-04-20 12:39:18 -0300

    Frontend React App.js remove unused imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import './App.css';
-import Header from './components/menu/index';
-import Background from './components/background/index';
-import UploadCard from './components/cardUpload/index';
-import SearchCard from './components/cardSearch/index';
 import Routes from './routes/routes';
 
 function App() {
