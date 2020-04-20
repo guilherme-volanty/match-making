307ac4252c49c88afbcee6f9e3c662ed1a0f8faf
moraesjeremias <moraesjeremias@gmail.com>
2020-04-20 12:35:20 -0300

    Api call methods add + baseURL change

--- frontend/src/services/uploadApi.js
+++ frontend/src/services/uploadApi.js
@@ -2,8 +2,40 @@ import axios from 'axios';
 
 
 const uploadApi = axios.create({
-    baseURL: 'http://localhost:4000'
+    baseURL: 'http://localhost:4002'
 });
 
+const getBrands = async () =>{
+    const response = await uploadApi.get('/base-cars-list/?key=brand') ;
+    return response 
+};
 
-export default uploadApi;
+const getModels = async () =>{
+    const response = await uploadApi.get('/base-cars-list/?key=model');
+    return response
+};
+
+const getYear = async () =>{
+    const response = await uploadApi.get('/base-cars-list?key=modelYear');
+    return response
+};
+
+const getBodyWork = async () =>{
+    const response = await uploadApi.get('/base-cars-list/?key=bodyWork');
+    return response
+};
+
+const getVersion = async () =>{
+    const response = await uploadApi.get('/base-cars-list/?key=version');
+    return response
+};
+
+const Api ={
+    getBrands,
+    getModels,
+    getYear,
+    getBodyWork,
+    getVersion
+}
+
+export default Api;
