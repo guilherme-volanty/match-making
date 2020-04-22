6753295aefe52fd67517108801ebf79c97a3773c
moraesjeremias <moraesjeremias@gmail.com>
2020-04-22 00:59:45 -0300

    Car Search and Filter Component add and usage

--- frontend/src/pages/SearchPage.js
+++ frontend/src/pages/SearchPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Menu from '../components/menu/index';
 import Background from '../components/background/index';
-import CarFilter from '../components/carFilter3/index' ;
+import CarFilter from '../components/carSearchFilter/index' ;
 
 const SearchPage = () => {
     return (
