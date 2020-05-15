b8e74ad4ad5d2288e9c0763757cb6eb50dbb2c29
moraesjeremias <moraesjeremias@gmail.com>
2020-05-14 22:46:34 -0300

    Roteamento para NGINX

--- server/routes/matchRouter.js
+++ server/routes/matchRouter.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 const MatchController = require("../Controllers/match-controller");
 
-router.get("/match/all", MatchController.getAllMatchs);
-router.get("/match/:id", MatchController.getMatchById);
-router.post("/match", MatchController.postNewMatch)
-router.delete("/match/delete/:id", MatchController.deleteMatchById);
-router.put("/match/update/:id", MatchController.updateMatchById);
+router.get("/matches/match/all", MatchController.getAllMatchs);
+router.get("/matches/match/:id", MatchController.getMatchById);
+router.post("/matches/match", MatchController.postNewMatch)
+router.delete("/matches/match/delete/:id", MatchController.deleteMatchById);
+router.put("/matches/match/update/:id", MatchController.updateMatchById);
 
 module.exports = router; 
