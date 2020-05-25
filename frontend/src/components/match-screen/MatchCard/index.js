94b5f3ed004a3ba238cb34cd4cc9b8fadc530f24
moraesjeremias <moraesjeremias@gmail.com>
2020-05-25 01:06:15 -0300

    Componente universal de Card

---
+++ frontend/src/components/match-screen/MatchCard/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import "./styles.css";
+import LoadingComponent from '../../misc/loading/index';
+
+const MatchCard = (props) => {
+
+    return (
+        <div className="card mb-3-match-screen" >
+            <div className="card-body text-dark">
+                <h5 className="card-title title ">{props.database}</h5>
+                <div className ="card-text ">
+                    {props.data._id?
+                    <div className="attributes">
+                        <div className="row-attributes">
+                            <div className="attribute1">
+                                <span className="attributeName">Marca</span>
+                                <span> {props.data.brand}</span>
+                            </div>
+                            <div className="attribute2">
+                                <span className="attributeName">Modelo</span>
+                                <span> {props.data.model}</span>
+                            </div>
+                        </div>
+                        <div className="row-attributes">
+                            <div className="attribute3">
+                                <span className="attributeName">Ano de Modelo</span>
+                                <span> {props.data.modelYear}</span>
+                            </div>
+                            <div className="attribute4">
+                                <span className="attributeName">Ano de Fabricação</span>
+                                <span> {props.data.factoryYear}</span>
+                            </div>
+                        </div>
+                        <div className="attribute5">
+                            {props.targetDatabase ?
+                                <div> 
+                                    <span className="attributeName"> Versão </span>
+                                    <span> {props.data.versionLocaliza} </span>
+                                </div>
+                                :<div>
+                                    <label htmlFor="nome">Versão</label>
+                                        <select className="form-control" placeholder="Selecione" >
+                                            <option defaultValue value = "">Não há Match</option>
+                                            {/* {console.log(props.version)} */}
+                                        {props.version.map((version, idx) => (
+                                            <option key={idx} value={version}>{version}</option>
+                                        ))}
+                                        </select>
+                                </div>}
+                        </div>
+                    </div>:<LoadingComponent />}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default MatchCard; 
